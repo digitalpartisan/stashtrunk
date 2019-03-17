086d953183eed8c0886fadb3a3067b18e37f8e9d
digitalpartisan <digital.partisan.86@gmail.com>
2019-03-17 12:08:25 -0700

    Switched to Jiffy and updated metadata

--- .scaffold/config.sh
+++ .scaffold/config.sh
@@ -1,4 +1,4 @@
 SCAFFOLD_MOD_NAME="StashTrunk"
 SCAFFOLD_MOD_GAME_PATH="K:\Steam\steamapps\common\Fallout 4"
 SCAFFOLD_MOD_DELIVERABLES_PATH="K:\FO4 Modding\Packaging"
-SCAFFOLD_MOD_DEPENDENCIES="Loggout,InjectTec,Chronicle,Spawny,SettlementLocationPicker"
+SCAFFOLD_MOD_DEPENDENCIES="Jiffy,InjectTec,Chronicle,Spawny"
